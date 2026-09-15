8e20dbece9833530bb07d573ff4015c0d020df3d
crab-ally <274044817+crab-ally@users.noreply.github.com>
2026-09-15 14:00:34 +0900

    [ignore] 주석 수정

--- ros2_ws/src/sample_call/sample_call/sample_call.py
+++ ros2_ws/src/sample_call/sample_call/sample_call.py
@@ -1,31 +1,36 @@
 #!/usr/bin/env python3
-import torch,rclpy #파이토치, ros 라이브러리 들어감
-from rclpy.node import Node #노드 클래스 주입
-from transformers import AutoTokenizer,AutoModelForCausalLM #AI 로컬 돌리는 라이브러리
-from guide_interfaces.srv import GuideLLM #인터페이스
+import torch,rclpy  # PyTorch와 ROS 2 Python 라이브러리
+from rclpy.node import Node # ROS 2 Node 클래스 가져오기
+from transformers import AutoTokenizer,AutoModelForCausalLM # 토크나이저와 언어 모델 클래스
+from guide_interfaces.srv import GuideLLM   # ROS 2 Service 인터페이스 가져오기
 
 class LLMService(Node):
     def __init__(self):
-        super().__init__("llm_service") #노드 이름 지정
-        model_path="/home/hkit/Drone-VLA-VLM-Control-Project-Arion-/models/Qwen3-0.6B" #모델 경로
+        super().__init__("llm_service")
+        model_path="/home/hkit/workspace/Guide/models/Qwen3-0.6B"
+
+        # Qwen3 토크나이저 & llm 모델 로드
         self.tokenizer=AutoTokenizer.from_pretrained(model_path,local_files_only=True)
-        #모델 호출
         self.model=AutoModelForCausalLM.from_pretrained(model_path,local_files_only=True,torch_dtype="auto",device_map="auto")
-        #여기서 토치 라이브러리 물린걸로 봐서 온디바이스 할 때 이걸로 gpu 물어야 함
+
+        # llm ROS 2 Service 서버 생성
         self.srv=self.create_service(GuideLLM,"llm",self.callback)
-        #서비스 서버 만들기
+
         self.get_logger().info("Qwen3-0.6B LLM Service Ready")
-        #로그 찍음
 
     def callback(self,request,response):
+        # 문자열(request.prompt)을 LLM이 사용하는 chat message 형식으로 변환
         messages=[{"role":"user","content":request.prompt}]
-        #리퀘스트 날라오면 이변수에 저장
+        # 메시지를 Qwen chat 형식으로 변환하고 토큰화한 뒤 모델 장치로 이동
         inputs=self.tokenizer.apply_chat_template(messages,add_generation_prompt=True,return_tensors="pt").to(self.model.device)
-        #메세지가 변환
+
+        # LLM 추론 수행
         with torch.no_grad():
             outputs=self.model.generate(inputs,max_new_tokens=256,do_sample=False)
+
+        # 생성된 토큰을 문자열로 디코딩하여 Service Response에 저장 > 차후 px4_msg 형태로
         response.response=self.tokenizer.decode(outputs[0][inputs.shape[-1]:],skip_special_tokens=True)
-        # 응답 생성에서 이걸 px4_msg 형태로 응답하게
+
         self.get_logger().info(f"질문: {request.prompt}")
         self.get_logger().info(f"답변: {response.response}")
         return response
