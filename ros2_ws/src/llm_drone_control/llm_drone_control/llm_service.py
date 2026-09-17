00e4649e0327138ab2fb25db950ed93d31d9ae46
crab-ally <274044817+crab-ally@users.noreply.github.com>
2026-09-17 14:34:52 +0900

    [feature] add finetuned lora weight to Qwen

--- ros2_ws/src/llm_drone_control/llm_drone_control/llm_service.py
+++ ros2_ws/src/llm_drone_control/llm_drone_control/llm_service.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python3
 from pathlib import Path
-import yaml,torch,rclpy
-import re
+import yaml,torch,rclpy,re
 from rclpy.node import Node
 from std_msgs.msg import String
 from transformers import AutoTokenizer,AutoModelForCausalLM
+from peft import PeftModel
 from guide_interfaces.srv import GuideLLM
 from ament_index_python.packages import get_package_share_directory
 from llm_drone_control.schema import DEFINE_SCHEMA,SYSTEM_PROMPT
@@ -18,9 +17,9 @@ class LLMService(Node):
             config=yaml.safe_load(f) or {}
 
         model_config=config.get("model",{})
-
         model_path=Path(model_config.get("path","~/Drone-VLA-VLM-Control-Project-Arion-/models/Qwen3-0.6B")).expanduser()
-        max_new_tokens=model_config.get("max_new_tokens",256)
+        lora_path=Path(model_config.get("lora_path","~/Drone-VLA-VLM-Control-Project-Arion-/models/finetuned_qwen3_drone_lora")).expanduser()
+        max_new_tokens=model_config.get("max_new_tokens",64)
         do_sample=model_config.get("do_sample",False)
         torch_dtype=model_config.get("torch_dtype","auto")
         device_map=model_config.get("device_map","auto")
@@ -29,16 +28,38 @@ class LLMService(Node):
             raise FileNotFoundError(f"모델 경로가 존재하지 않습니다: {model_path}")
 
         self.tokenizer=AutoTokenizer.from_pretrained(model_path,local_files_only=True)
-        self.model=AutoModelForCausalLM.from_pretrained(model_path,local_files_only=True,torch_dtype=torch_dtype,device_map=device_map)
+
+        self.model=AutoModelForCausalLM.from_pretrained(
+            model_path,
+            local_files_only=True,
+            torch_dtype=torch_dtype,
+            device_map=device_map
+        )
+
+        if lora_path:
+            if not lora_path.exists():
+                raise FileNotFoundError(f"LoRA 경로가 존재하지 않습니다: {lora_path}")
+            self.model=PeftModel.from_pretrained(
+                self.model,
+                lora_path,
+                local_files_only=True
+            )
+
+        self.model.eval()
+
+        # warning 제거
+        self.model.generation_config.do_sample=False
+        self.model.generation_config.temperature=None
+        self.model.generation_config.top_p=None
+        self.model.generation_config.top_k=None
 
         self.max_new_tokens=max_new_tokens
         self.do_sample=do_sample
 
-        # publiser & service
         self.response_pub=self.create_publisher(String,"llm_response",10)
         self.srv=self.create_service(GuideLLM,"llm",self.callback)
 
-        self.get_logger().info("Qwen3-0.6B LLM Service Ready")
+        self.get_logger().info("Qwen3-0.6B + LoRA LLM Service Ready")
 
     def callback(self,request,response):
         messages=[
@@ -59,15 +80,23 @@ class LLMService(Node):
             outputs=self.model.generate(
                 **inputs,
                 max_new_tokens=self.max_new_tokens,
-                do_sample=self.do_sample,
-                temperature=None,
-                top_p=None,
-                top_k=None
+                do_sample=self.do_sample
             )
 
-        result=self.tokenizer.decode(outputs[0][inputs["input_ids"].shape[-1]:],skip_special_tokens=True)
-        matches=re.findall(r"<tool_call>\s*(.*?)\s*</tool_call>",result,re.DOTALL)
-        response.response="\n".join(f"<tool_call>{m}</tool_call>" for m in matches)
+        result=self.tokenizer.decode(
+            outputs[0][inputs["input_ids"].shape[-1]:],
+            skip_special_tokens=True
+        )
+
+        matches=re.findall(
+            r"<tool_call>\s*(.*?)\s*</tool_call>",
+            result,
+            re.DOTALL
+        )
+
+        response.response="\n".join(
+            f"<tool_call>{m}</tool_call>" for m in matches
+        )
 
         msg=String()
         msg.data=response.response
